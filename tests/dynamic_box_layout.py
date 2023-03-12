549e06d3178fe081385ed85df3f8d5d485aa954e
Theo Molteno <theo@molteno.net>
2023-03-12 14:16:41 +1300

    pretty much the desired behaviour

--- tests/dynamic_box_layout.py
+++ tests/dynamic_box_layout.py
@@ -5,12 +5,13 @@ from PyQt6.QtCore import Qt, QSize
 class MainWindow(QWidget):
 
     column_width = 150
+    num_columns = 3
 
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('Resizable Layout Example')
-        self.setMinimumSize(QSize(300, 200))
+        self.setMinimumSize(QSize(self.num_columns * self.column_width, 200))
 
         self.vbox = QVBoxLayout()
         self.setLayout(self.vbox)
@@ -18,16 +19,30 @@ class MainWindow(QWidget):
         self.grid = QGridLayout()
         self.vbox.addLayout(self.grid)
 
+        self.add_widgets()
+
+    def remove_widgets(self):
+        while self.grid.count():
+            item = self.grid.takeAt(0)
+            widget = item.widget()
+            if widget is not  None:
+                widget.deleteLater()
+
+    def add_widgets(self):
+        self.remove_widgets()
         for i in range(20):
             btn = QPushButton(f'Button {i}')
-            self.grid.addWidget(btn, i // 4, i % 4)
-
+            self.grid.addWidget(btn, i // self.num_columns, i % self.num_columns)
 
     def resizeEvent(self, event):
         """this is automatically called on resize (it overrides the parent)"""
         # Update the number of columns based on the width of the window
         columns = event.size().width() // self.column_width
-        print(f"columns {columns}")
+        if columns != self.num_columns:
+            print(f"updating number of columns from {self.num_columns} to {columns}")
+
+            self.num_columns = columns
+            self.add_widgets()
 
 
 if __name__ == '__main__':
